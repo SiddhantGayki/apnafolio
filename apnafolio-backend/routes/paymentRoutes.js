81108240e924db6e7f609f13f39031757611230f
SIDDHANT GAYKI <siddhantgayki03@gmail.com>
2025-10-01 21:35:20 +0530

    Update paymentRoutes.js

--- apnafolio-backend/routes/paymentRoutes.js
+++ apnafolio-backend/routes/paymentRoutes.js
@@ -13,6 +13,16 @@ module.exports = router;
 // const auth = require("../middleware/auth");
 // const { createOrder, verifyPayment } = require("../controllers/paymentController");
 
+// router.post("/order", auth, createOrder);
+// router.post("/verify", auth, verifyPayment);
+
+// module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const auth = require("../middleware/auth");
+// const { createOrder, verifyPayment } = require("../controllers/paymentController");
+
 // router.post("/order", createOrder);
 // router.post("/verify", verifyPayment);
 
