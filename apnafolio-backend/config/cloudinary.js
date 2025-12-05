998b600408c895063956a61a35ccc71b0618d5fe
SIDDHANT GAYKI <siddhantgayki03@gmail.com>
2025-12-05 21:18:58 +0530

    Refactor Cloudinary setup and add multer storage
    Updated Cloudinary configuration and added multer storage for file uploads.

--- apnafolio-backend/config/cloudinary.js
+++ apnafolio-backend/config/cloudinary.js
@@ -1,9 +1,32 @@
+// middleware/upload.js
+const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_KEY,
+  api_secret: process.env.CLOUD_SECRET,
 });
 
-module.exports = cloudinary;
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "apnafolio",
+    resource_type: "auto",
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
+
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
+
+// module.exports = cloudinary;
